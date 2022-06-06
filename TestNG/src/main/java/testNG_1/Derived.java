4c7a91f7423d15b6690db6bd887e31e040217a5e
CrioUser <crio-user@criodo.com>
2022-06-06 18:34:28 +0530

    [src/main] Add file for session demo

---
+++ TestNG/src/main/java/testNG_1/Derived.java
@@ -0,0 +1,20 @@
+package testNG_1;
+
+// Derived class
+class Derived extends Base {
+
+    @Override
+    public void display(int x) {
+        // Print statement when this method is called
+        System.out.println("Derived display(int)");
+    }
+
+    // Invoke the display method
+    public static void main(String args[]) {
+        // Creating object of this class inside main()
+        Derived obj = new Derived();
+
+        // *NOTE* - Will execute base display() method
+        obj.display();
+    }
+}
